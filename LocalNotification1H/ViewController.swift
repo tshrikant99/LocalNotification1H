c49495b4341a1d620fa7ce48354ad94889f8e739
Shrikant Talegaonkar <shrikant.t@codewalla.com>
2021-12-14 15:38:31 +0530

    xcode project base files

---
+++ LocalNotification1H/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalNotification1H
+//
+//  Created by shrikant on 14/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
